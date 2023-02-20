96b88efb1eabbf229f09dc2a5b52318fba77f999
@a-mendes-1975 <ademilsonmendes1307@gmail.com>
2023-02-20 01:11:37 -0300

    atualizacao

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace areaTriangulo
+{
+
+    class AreaTriangulo
+    {
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("teste");
+
+        }
+    }
+}
