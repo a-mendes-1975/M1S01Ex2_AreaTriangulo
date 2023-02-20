2cde6d72e1ea7c1676cc965f0c07ef019ef51088
@a-mendes-1975 <ademilsonmendes1307@gmail.com>
2023-02-20 13:43:23 -0300

    projeto final

--- Program.cs
+++ Program.cs
@@ -1,23 +1,27 @@
 ﻿using System;
 
-/* 
-[M1S01] Ex 2 - Área do Triângulo 
-[Variáveis]
-Crie um programa que receba os valores da base e da altura de um triângulo qualquer, 
-e calcule a área do triângulo usando a fórmula area =(base × altura)/2 e exiba o resultado para o usuário: 
-Saída: A área do triângulo é {area}
-
-*/
-
 namespace areaTriangulo
 {
 
-    class AreaTriangulo
+    class areaTriangulo
     {
 
         static void Main(string[] args)
         {
-            System.Console.WriteLine("teste");
+            /* 
+            [M1S01] Ex 2 - Área do Triângulo 
+            [Variáveis]
+            Crie um programa que receba os valores da base e da altura de um triângulo qualquer, 
+            e calcule a área do triângulo usando a fórmula area =(base × altura)/2 e exiba o resultado para o usuário: 
+            Saída: A área do triângulo é {area}
+            */
+
+            System.Console.WriteLine("Digite a Base do Triângulo: ");
+            int baseTriangulo = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("Digite a altura do Triângulo: ");
+            int altura = int.Parse(Console.ReadLine());
+            int area = (baseTriangulo * altura) / 2;           
+            System.Console.WriteLine("A área do triângulo é " + area);
 
         }
     }
