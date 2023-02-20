62309d1ca1ff911a19d22bf80d57d8e4910dba20
@a-mendes-1975 <ademilsonmendes1307@gmail.com>
2023-02-20 01:14:55 -0300

    atualização descrição exercicio

--- Program.cs
+++ Program.cs
@@ -1,5 +1,14 @@
 ﻿using System;
 
+/* 
+[M1S01] Ex 2 - Área do Triângulo 
+[Variáveis]
+Crie um programa que receba os valores da base e da altura de um triângulo qualquer, 
+e calcule a área do triângulo usando a fórmula area =(base × altura)/2 e exiba o resultado para o usuário: 
+Saída: A área do triângulo é {area}
+
+*/
+
 namespace areaTriangulo
 {
 
